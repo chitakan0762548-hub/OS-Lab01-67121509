9dba9676fdf031c33e7a00dd2e7259c6cd87dbd5
chitakan0762548-hub <chitakan0762548@gmail.com>
2026-08-31 14:24:53 +0000

    Completed Lab 1

--- profiler.py
+++ profiler.py
@@ -8,3 +8,6 @@ import psutil
 
 # TODO: Write your system profiler code here 
 # Follow the instructions in the Lab manual.
+print(f"OS Name: {platform.system()} {platform.release()}")
+print(f"Number of CPU Cores: {psutil.cpu_count(logical=True)}")
+print(f"Total RAM: {round(psutil.virtual_memory().total / (1024**3), 2)} GB")
